f8513118fb682e3a98b65cf7223ec6957e6d714f
tequilex <dron2010xxx@mail.ru>
2024-02-06 16:46:28 +0300

    added redux toolkit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyB-K42Q6ZUtYjgygNY5M9NCZApzZ_aXeGA",
+  authDomain: "crwn-tequilex-db.firebaseapp.com",
+  projectId: "crwn-tequilex-db",
+  storageBucket: "crwn-tequilex-db.appspot.com",
+  messagingSenderId: "142549372342",
+  appId: "1:142549372342:web:30147437be9fbcc010c517"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
